8dba8032d8720c29fea11e0c3e7535236bfb7cf5
Admin <5469212+kondratovbr@users.noreply.github.com>
2022-02-18 23:05:10 +0300

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,18 +7,19 @@ mix.setPublicPath('source/assets/build');
 mix
     .webpackConfig({ stats: { children: true }})
     .js('source/_assets/js/main.js', 'js')
-    .postCss('source/_assets/css/main.pcss', 'css/main.css', [
-        require('postcss-import'),
-        require('tailwindcss/nesting'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ])
-    .options({
-        processCssUrls: false,
-    })
+    .postCss('source/_assets/css/main.pcss', 'css/main.css')
     .jigsaw({
         browserSync: false,
         watch: ['config.php', 'source/**/*.md', 'source/**/*.php', 'source/**/*.pcss'],
+    })
+    .options({
+        processCssUrls: false,
+        postCss: [
+            require('postcss-import'),
+            require('tailwindcss/nesting'),
+            require('tailwindcss'),
+            require('autoprefixer'),
+        ],
     })
     .version();
 
