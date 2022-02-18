fb4121fd6df395a3b63d57dee7c54a597ce4faad
Admin <5469212+kondratovbr@users.noreply.github.com>
2022-02-18 16:07:23 +0300

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -1,6 +1,41 @@
 <?php
 
 return [
+    'Introduction' => [
+        'url' => 'docs/introduction',
+    ],
+
+    'Account' => [
+        'children' => [
+            'Security' => 'account/security',
+            'Source Control' => 'account/vcs',
+            'SSH Keys' => 'account/ssh',
+            'Billing' => 'account/billing',
+        ],
+    ],
+
+    'Servers' => [
+        'children' => [
+            'Server Providers' => 'servers/providers',
+            'Server Types' => 'servers/types',
+            'Git Access' => 'servers/git',
+            'Python' => 'servers/python',
+            'Nginx Configuration' => 'servers/nginx',
+            'Security' => 'servers/security',
+            'Databases' => 'servers/databases',
+            'Daemons' => 'servers/daemons',
+        ],
+    ],
+
+    'Projects' => [
+        'children' => [
+            'Basics' => 'projects/basics',
+            'Deployment' => 'projects/deployment',
+            'Queues' => 'projects/queues',
+            'SSL' => 'projects/ssl',
+        ],
+    ],
+
     'Getting Started' => [
         'url' => 'docs/getting-started',
         'children' => [
@@ -10,5 +45,6 @@ return [
             'Custom 404 Page' => 'docs/custom-404-page',
         ],
     ],
+
     'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
 ];
