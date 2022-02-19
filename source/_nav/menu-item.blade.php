883371c0b57e1e7967ab66a29a1030f386fcd3d3
Admin <5469212+kondratovbr@users.noreply.github.com>
2022-02-19 18:18:12 +0300

    Re-style nav menu

--- source/_nav/menu-item.blade.php
+++ source/_nav/menu-item.blade.php
@@ -1,4 +1,4 @@
-<li class="pl-4">
+<li class="pl-4 {{ 'li-lvl' . $level }}">
     @if ($url = is_string($item) ? $item : $item->url)
         {{-- Menu item with URL--}}
         <a href="{{ $page->url($url) }}"
@@ -8,10 +8,10 @@
         </a>
     @else
         {{-- Menu item without URL--}}
-        <p class="nav-menu__item text-gray-600">{{ $label }}</p>
+        <p class="nav-menu__item text-gray-400 {{ 'lvl' . $level }}">{{ $label }}</p>
     @endif
 
-    @if (! is_string($item) && $item->children)
+    @if ( ! is_string($item) && $item->children)
         {{-- Recursively handle children --}}
         @include('_nav.menu', ['items' => $item->children, 'level' => ++$level])
     @endif
