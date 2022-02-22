312bfc6af22b8e8fe3783ede0ed6a7dc1f6a2412
Admin <5469212+kondratovbr@users.noreply.github.com>
2022-02-22 16:15:23 +0300

    Bugfix message block parser

--- app/Extensions/Message/MessageBlockContinueParser.php
+++ app/Extensions/Message/MessageBlockContinueParser.php
@@ -3,19 +3,15 @@
 namespace App\Extensions\Message;
 
 use League\CommonMark\Node\Block\AbstractBlock;
-use League\CommonMark\Node\Block\Paragraph;
 use League\CommonMark\Parser\Block\AbstractBlockContinueParser;
 use League\CommonMark\Parser\Block\BlockContinue;
 use League\CommonMark\Parser\Block\BlockContinueParserInterface;
-use League\CommonMark\Parser\Block\BlockContinueParserWithInlinesInterface;
 use League\CommonMark\Parser\Cursor;
-use League\CommonMark\Parser\InlineParserEngineInterface;
 
-class MessageBlockContinueParser extends AbstractBlockContinueParser implements BlockContinueParserWithInlinesInterface
+class MessageBlockContinueParser extends AbstractBlockContinueParser
 {
     private string $syntax;
     private Message $block;
-    private array $lines = [];
 
     public function __construct(string|null $style, string $syntax)
     {
@@ -28,16 +24,6 @@ class MessageBlockContinueParser extends AbstractBlockContinueParser implements 
     public function isContainer(): bool
     {
         return true;
-    }
-
-    public function canHaveLazyContinuationLines(): bool
-    {
-        return true;
-    }
-
-    public function addLine(string $line): void
-    {
-        $this->lines[] = $line;
     }
 
     public function canContain(AbstractBlock $childBlock): bool
@@ -66,16 +52,5 @@ class MessageBlockContinueParser extends AbstractBlockContinueParser implements 
         $cursor->restoreState($savedState);
 
         return BlockContinue::at($cursor);
-    }
-
-    public function parseInlines(InlineParserEngineInterface $inlineParser): void
-    {
-        foreach ($this->lines as $line) {
-            $paragraph = new Paragraph;
-
-            $inlineParser->parse($line, $paragraph);
-
-            $this->block->appendChild($paragraph);
-        }
     }
 }
