7d11471c15a88462d60861b763683e1f1f76fee9
Admin <5469212+kondratovbr@users.noreply.github.com>
2022-03-10 16:46:50 +0300

    Cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,8 +67,6 @@ module.exports = {
                 loose: '1.75',
             },
             maxWidth: {
-                none: 'none',
-                '7xl': '80rem',
                 '8xl': '88rem'
             },
             spacing: {
