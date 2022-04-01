96aee929174c112a9a57de78cefa82914240b6a3
Admin <5469212+kondratovbr@users.noreply.github.com>
2022-04-01 18:19:58 +0300

    Cleanup

--- navigation.php
+++ navigation.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+
     'Introduction' => [
         'url' => 'introduction',
     ],
@@ -35,15 +36,4 @@ return [
         ],
     ],
 
-    'Getting Started' => [
-        'url' => 'docs/getting-started',
-        'children' => [
-            'Customizing Your Site' => 'docs/customizing-your-site',
-            'Navigation' => 'docs/navigation',
-            'Algolia DocSearch' => 'docs/algolia-docsearch',
-            'Custom 404 Page' => 'docs/custom-404-page',
-        ],
-    ],
-
-    'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
 ];
