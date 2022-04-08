80792d8aa7df036dc895562957abbdf46fbea5cc
Admin <5469212+kondratovbr@users.noreply.github.com>
2022-04-08 15:09:33 +0300

    Update favicons

--- source/_partials/favicons.blade.php
+++ source/_partials/favicons.blade.php
@@ -4,8 +4,8 @@
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 <link rel="manifest" href="/site.webmanifest">
 <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#002952">
-<meta name="apple-mobile-web-app-title" content="Michman">
-<meta name="application-name" content="Michman">
+<meta name="apple-mobile-web-app-title" content="Michman Docs">
+<meta name="application-name" content="Michman Docs">
 <meta name="msapplication-TileColor" content="#002952">
 <meta name="msapplication-TileImage" content="/mstile-144x144.png">
 <meta name="theme-color" content="#002952">
