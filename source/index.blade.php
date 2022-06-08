45792e095a1dd508c48a4a3f02ba86eed3c8ffb6
Admin <5469212+kondratovbr@users.noreply.github.com>
2022-06-08 17:24:47 +0200

    Fix index page redirect

--- source/index.blade.php
+++ source/index.blade.php
@@ -7,7 +7,7 @@ sitemap: false
     <head>
         <meta charset="UTF-8">
         <title>Redirecting to homepage {{ $page->fullUrl($page->homepage) }}</title>
-        <meta http-equiv="refresh" content="0; URL={{ $page->fullUrl($page->homepage) }}">
+        <meta http-equiv="refresh" content="0; URL={{ $page->url($page->homepage) }}">
         <link rel="canonical" href="{{ $page->fullUrl($page->homepage) }}">
     </head>
 </html>
