178de7bd0a1cea140dc0d19225825ead939e46e7
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-11-21 14:12:26 +0100

    Update socials.blade.php

--- source/_partials/socials.blade.php
+++ source/_partials/socials.blade.php
@@ -1,13 +1,3 @@
-{{-- Generic Social Tags --}}
-<meta property="og:site_name" content="{{ $page->siteName }}">
-<meta property="og:title" content="{{ $page->title ?? $page->siteName }}">
-<meta property="og:description" content="{{ $page->description ?? $page->siteDescription }}">
-<meta property="og:url" content="{{ $page->getUrl() }}">
-<meta property="og:image" content="{{ $page->imageFullUrl('twitter-og.png') }}">
-<meta property="og:image:alt" content="{{ $page->siteName }}">
-<meta property="og:type" content="website">
-<meta property="og:locale" content="en_US">
-
 {{-- Twitter-specific Tags --}}
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:image" content="{{ $page->imageFullUrl('twitter-og.png') }}">
@@ -18,6 +8,16 @@
 <meta name="twitter:description" content="{{ $page->description ?? $page->siteDescription }}">
 {{--<meta name="twitter:label2" value="API">--}}
 {{--<meta name="twitter:data2" value="https://docs.michman.dev/api">--}}
+
+{{-- Generic Social Tags --}}
+<meta property="og:site_name" content="{{ $page->siteName }}">
+<meta property="og:title" content="{{ $page->title ?? $page->siteName }}">
+<meta property="og:description" content="{{ $page->description ?? $page->siteDescription }}">
+<meta property="og:url" content="{{ $page->getUrl() }}">
+<meta property="og:image" content="{{ $page->imageFullUrl('twitter-og.png') }}">
+<meta property="og:image:alt" content="{{ $page->siteName }}">
+<meta property="og:type" content="website">
+<meta property="og:locale" content="en_US">
 
 {{-- Facebook Tags --}}
 {{--<meta property="fb:app_id" content="1234567890">--}}
